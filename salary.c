1afb8add9e26b323313ca54871f0104bd19a23e9
Cynthia5621 <muthonicynthia095@gmail.com>
2024-10-02 13:33:46 +0300

    Add files via upload

---
+++ salary.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main () {
+	int age;
+	float salary;
+	printf("enter the age");
+	scanf("%d",&age);
+	printf("enter salary")
+	scanf("%f",&salary);
+	if (age>=21&&salary>=21000){
+	printf("congratulations you qualify for the loan");	
+	}else{
+    printf(unfortunately we are unable to offer you a loan at this time")
+	}
+	return 0:
+}
+	
+	
